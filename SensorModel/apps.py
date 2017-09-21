492c5bd0ba6e34bbd551be0ef5dfd371c66637cf
Baron Hou <houbaron@gmail.com>
2017-09-21 18:21:55 +0800

    init

---
+++ SensorModel/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SensormodelConfig(AppConfig):
+    name = 'SensorModel'
---
+++ SensorTypeModel/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SensortypemodelConfig(AppConfig):
+    name = 'SensorTypeModel'
